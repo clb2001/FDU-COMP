59e7d5a9ad3911b8c769635d2d7772b6906cb8bd
Libin Cheng <clb245000@163.com>
2023-09-05 20:15:52 +0800

    del

--- homework/homework5/leaf.cpp
+++
@@ -1,9 +0,0 @@
-int BinaryTree<Type>::leaf(BinTreeNode<Type> *ptr)
-{
-    if (ptr == NULL)
-        return 0;
-    else if (ptr->leftChild == NULL && ptr->rightChild == NULL)
-        return 1;
-    else
-        return leaf(ptr->leftChild) + leaf(ptr->rightChild);
-}
